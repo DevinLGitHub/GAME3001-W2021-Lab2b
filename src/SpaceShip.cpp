a7b23f2fee59b4ee9138d2d0cfa7e930822c3f08
DevinLGitHub <devin.lau.mackenzie@gmail.com>
2021-01-26 16:52:18 -0500

    Modified setRotation so it works with Orientation

--- src/SpaceShip.cpp
+++ src/SpaceShip.cpp
@@ -1,5 +1,7 @@
 #include "SpaceShip.h"
 
+
+#include "Game.h"
 #include "Util.h"
 
 SpaceShip::SpaceShip()
@@ -82,7 +84,15 @@ void SpaceShip::setOrientation(glm::vec2 orientation)
 
 void SpaceShip::setRotation(float angle)
 {
+	// angle to degrees
 	m_rotationAngle = angle;
+	auto angle_in_radians = (angle - 90.0f) * Util::Deg2Rad;
+
+	auto x = cos(angle_in_radians);
+	auto y = sin(angle_in_radians);
+	
+	// convert the angle to a normalized vector and store it in Orientation
+	setOrientation(glm::vec2(x, y));
 }
 
 float SpaceShip::getRotation() const
@@ -92,13 +102,18 @@ float SpaceShip::getRotation() const
 
 void SpaceShip::m_Move()
 {
+	auto deltaTime = TheGame::Instance()->getDeltaTime();
+	
 	// direction with magnitude
 	m_targetDirection = m_destination - getTransform()->position;
 	
 	// normalized direction
 	m_targetDirection = Util::normalize(m_targetDirection);
 
-	getRigidBody()->velocity = m_targetDirection * m_maxSpeed;
+	auto target_rotation = Util::signedAngle(getOrientation(), m_targetDirection);
+	std::cout << "Target Rotation: " << target_rotation << std::endl;
+		
+	//getRigidBody()->velocity = m_targetDirection * m_maxSpeed;
 
-	getTransform()->position += getRigidBody()->velocity;
+	//getTransform()->position += getRigidBody()->velocity;
 }
